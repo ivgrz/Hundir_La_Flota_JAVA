1840d314ea167e23775c673b57ded17eb1b1d92f
ivgrz <igutierrezcontreras@danielcastelao.org>
2026-04-20 14:27:06 +0200

    Clase_Nave

---
+++ Migracion_Hundir_La_Flota/src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Nave n = new Nave("bote","submarino",4,8);
+        n.restarvida();
+        System.out.println(n);
+    }
+
+}
