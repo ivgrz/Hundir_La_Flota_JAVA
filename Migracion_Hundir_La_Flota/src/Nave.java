02dfc8a7c1e37628479cbc63c9c2181e664ec1af
ivgrz <igutierrezcontreras@danielcastelao.org>
2026-04-20 14:11:46 +0200

    Commit_inicial

---
+++ Migracion_Hundir_La_Flota/src/Nave.java
@@ -0,0 +1,12 @@
+public class Nave {
+    public String nombre;
+    public String tipo;
+    public int tamano;
+    public Nave(String nombre, String tipo, int tamano){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.tamano = tamano;
+
+    }
+
+}
